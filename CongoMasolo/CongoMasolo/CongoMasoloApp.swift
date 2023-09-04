4ed36ad991656af61ccd683f611cc99b0a5573bb
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-09-04 20:07:39 +0200

    Initial Project

---
+++ CongoMasolo/CongoMasolo/CongoMasoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  CongoMasoloApp.swift
+//  CongoMasolo
+//
+//  Created by Cédric Bahirwe on 04/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CongoMasoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
